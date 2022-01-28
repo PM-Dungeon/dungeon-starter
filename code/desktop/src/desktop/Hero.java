8983772258709fa006ff55fb8990ab7abf5c4a2b
Tobias Grothe <85501570+Tobias321@users.noreply.github.com>
2022-01-28 15:53:00 +0100

    Temporary commit to test gradlew verGJF

---
+++ code/desktop/src/desktop/Hero.java
@@ -0,0 +1,87 @@
+package desktop;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import graphic.Animation;
+import graphic.Painter;
+import interfaces.IAnimatable;
+import level.elements.Level;
+import tools.Point;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Hero implements IAnimatable {
+    private SpriteBatch batch;
+    private Painter painter;
+
+    private Point position;
+
+    private Level level;
+
+    private Animation idleAnimation;
+
+    public Hero(SpriteBatch batch, Painter painter) {
+        // Test comment
+        List<String> idle = new ArrayList<>();
+
+        idle.add("character/knight/knight_m_idle_anim_f0.png");
+        idle.add("character/knight/knight_m_idle_anim_f1.png");
+
+        idleAnimation = new Animation(idle, 8);
+
+        this.batch = batch;
+        this.painter = painter;
+    }
+
+    @Override
+    public Animation getActiveAnimation() {
+        return this.idleAnimation;
+    }
+
+    @Override
+    public void update() {
+
+        Point newPosition = new Point(this.position);
+
+        float movementSpeed = 0.1f;
+
+        if (Gdx.input.isKeyPressed(Input.Keys.W)) newPosition.y += movementSpeed;
+
+        if (Gdx.input.isKeyPressed(Input.Keys.S)) newPosition.y -= movementSpeed;
+
+        if (Gdx.input.isKeyPressed(Input.Keys.D)) newPosition.x += movementSpeed;
+
+        if (Gdx.input.isKeyPressed(Input.Keys.A)) newPosition.x -= movementSpeed;
+
+        this.position = newPosition;
+
+        this.draw();
+    }
+
+    @Override
+    public Point getPosition() {
+        return this.position;
+    }
+
+    @Override
+    public SpriteBatch getBatch() {
+        return batch;
+    }
+
+    @Override
+    public Painter getGraphicController() {
+        return painter;
+    }
+
+    @Override
+    public boolean removable() {
+        return false;
+    }
+
+    public void setLevel(Level level) {
+        this.level = level;
+        this.position = level.getStartTile().getGlobalPosition();
+    }
+}
---
+++ code/desktop/src/desktop/MyGame.java
@@ -0,0 +1,34 @@
+package desktop;
+
+import controller.MainController;
+
+public class MyGame extends MainController {
+    private Hero hero;
+
+    @Override
+    public void setup() {
+
+        hero = new Hero(batch, painter);
+
+        entityController.add(hero);
+
+        camera.follow(hero);
+
+        levelAPI.loadLevel();
+    }
+
+    @Override
+    protected void beginFrame() {}
+
+    @Override
+    protected void endFrame() {}
+
+    @Override
+    public void onLevelLoad() {
+        hero.setLevel(levelAPI.getCurrentLevel());
+    }
+
+    public static void main(String[] args) {
+        DesktopLauncher.run(new MyGame());
+    }
+}
