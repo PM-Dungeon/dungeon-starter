f36697d1f91579c4e37ff7548c4ff91b3c12f17c
Tobias Grothe <85501570+Tobias321@users.noreply.github.com>
2022-01-30 09:21:52 +0100

    DesktopLauncher: Remove main method

--- code/desktop/src/desktop/DesktopLauncher.java
+++ code/desktop/src/desktop/DesktopLauncher.java
@@ -4,7 +4,6 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import controller.LibgdxSetup;
 import controller.MainController;
-import demo.MyController;
 import tools.Constants;
 
 public final class DesktopLauncher {
@@ -14,9 +13,5 @@ public final class DesktopLauncher {
         config.height = Constants.WINDOW_HEIGHT;
         config.foregroundFPS = Constants.FRAME_RATE;
         new LwjglApplication(new LibgdxSetup(mc), config);
-    }
-
-    public static void main(String[] args) {
-        DesktopLauncher.run(new MyController());
     }
 }
