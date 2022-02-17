6dd814ca00f5022c1f97339d0887466db85042dd
AHeinisch <alexander.heinisch@fh-bielefeld.de>
2022-02-17 10:49:46 +0100

    fix formatting

--- code/desktop/src/com/badlogic/gdx/backends/lwjgl/DungeonApplication.java
+++ code/desktop/src/com/badlogic/gdx/backends/lwjgl/DungeonApplication.java
@@ -3,8 +3,8 @@ package com.badlogic.gdx.backends.lwjgl;
 import com.badlogic.gdx.ApplicationListener;
 
 /**
- * An extended LwjglApplication which allows to define the Statuscode, which should be returned on forceExit.
- *
+ * An extended LwjglApplication which allows to define the Statuscode, which should be returned on
+ * forceExit.
  */
 public class DungeonApplication extends LwjglApplication {
 
@@ -16,11 +16,13 @@ public class DungeonApplication extends LwjglApplication {
      *
      * @param listener
      * @param config
-     * @param statuscode The Exitcode which the Operating System needs. Some macOS versions may need -1 to successfully shutdown.
+     * @param statuscode The Exitcode which the Operating System needs. Some macOS versions may need
+     *     -1 to successfully shutdown.
      */
-    public DungeonApplication(ApplicationListener listener, LwjglApplicationConfiguration config, int statuscode) {
+    public DungeonApplication(
+            ApplicationListener listener, LwjglApplicationConfiguration config, int statuscode) {
         super(listener, config);
-        if(config.forceExit){
+        if (config.forceExit) {
             this.graphics.config.forceExit = false;
             this.forceExit = true;
             this.statusCode = statuscode;
@@ -30,7 +32,7 @@ public class DungeonApplication extends LwjglApplication {
     @Override
     protected void mainLoop() {
         super.mainLoop();
-        if(forceExit){
+        if (forceExit) {
             System.exit(statusCode);
         }
     }
