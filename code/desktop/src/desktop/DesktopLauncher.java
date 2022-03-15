0b64152d677f3a6b971c9f581eccbf2b1e0ba214
jedi101 <mythos007@gmx.de>
2022-03-15 14:40:11 +0100

    typo fixed in constant name

--- code/desktop/src/desktop/DesktopLauncher.java
+++ code/desktop/src/desktop/DesktopLauncher.java
@@ -12,7 +12,7 @@ public final class DesktopLauncher {
         config.width = Constants.WINDOW_WIDTH;
         config.height = Constants.WINDOW_HEIGHT;
         config.foregroundFPS = Constants.FRAME_RATE;
-        config.title = Constants.WINDOW.TITLE;
+        config.title = Constants.WINDOW_TITLE;
         config.addIcon("logo/logo.png", Files.FileType.Internal);
         new DungeonApplication(new LibgdxSetup(mc), config, 0);
     }
