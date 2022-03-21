eca6017a71175c30620c9876624fc0e5d42f0ef4
Jd <33090297+jedi101@users.noreply.github.com>
2022-03-21 22:16:06 +0100

    Add constant window title and 32px logo (#57)
    * Added constant window title and 32px logo
    
    * Add Logo path from constants into DesktopLauncher
    
    Co-authored-by: jedi101 <mythos007@gmx.de>

--- code/desktop/src/desktop/DesktopLauncher.java
+++ code/desktop/src/desktop/DesktopLauncher.java
@@ -1,5 +1,6 @@
 package desktop;
 
+import com.badlogic.gdx.Files;
 import com.badlogic.gdx.backends.lwjgl.DungeonApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import controller.LibgdxSetup;
@@ -12,6 +13,8 @@ public final class DesktopLauncher {
         config.width = Constants.WINDOW_WIDTH;
         config.height = Constants.WINDOW_HEIGHT;
         config.foregroundFPS = Constants.FRAME_RATE;
+        config.title = Constants.WINDOW_TITLE;
+        config.addIcon(Constants.LOGO_PATH, Files.FileType.Internal);
         new DungeonApplication(new LibgdxSetup(mc), config, 0);
     }
 }
