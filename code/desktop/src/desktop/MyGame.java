df62a6d5ec15729ec14d5e6cccd05dd7243c9332
Andre Matutat <amatutat@web.de>
2022-04-09 16:45:49 +0200

    use LevelLoader

--- code/desktop/src/desktop/MyGame.java
+++ code/desktop/src/desktop/MyGame.java
@@ -2,31 +2,35 @@ package desktop;
 
 import com.badlogic.gdx.Gdx;
 import controller.MainController;
+import level.generator.LevelLoader.LevelLoader;
 import level.generator.dungeong.graphg.NoSolutionException;
 
 public class MyGame extends MainController {
 
     @Override
     protected void setup() {
-
+        levelAPI.setGenerator(new LevelLoader());
         // load the first level
         try {
             levelAPI.loadLevel();
         } catch (NoSolutionException e) {
             System.out.println(
-                    "Es konnte kein Level geladen werden, bitte den \"assets\" Ordner überprüfen.");
+                "Es konnte kein Level geladen werden, bitte den \"assets\" Ordner überprüfen.");
             Gdx.app.exit();
         }
     }
 
     @Override
-    protected void beginFrame() {}
+    protected void beginFrame() {
+    }
 
     @Override
-    protected void endFrame() {}
+    protected void endFrame() {
+    }
 
     @Override
-    public void onLevelLoad() {}
+    public void onLevelLoad() {
+    }
 
     public static void main(String[] args) {
         // start the game
