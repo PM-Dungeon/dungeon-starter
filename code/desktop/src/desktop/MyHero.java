3d3443e00ec9606a4d26d2fede1c970aff0f80d2
jedi101 <mythos007@gmx.de>
2022-04-11 13:43:21 +0200

    Added position to MyHero

--- code/desktop/src/desktop/MyHero.java
+++ code/desktop/src/desktop/MyHero.java
@@ -3,19 +3,27 @@ package desktop;
 import basiselements.Entity;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import graphic.Painter;
+import level.elements.Level;
 import tools.Point;
 
 public class MyHero extends Entity {
     private String texturePath;
+    private Point position;
+    private Level currentLevel;
 
     public MyHero(Painter painter, SpriteBatch batch) {
         super(painter, batch);
-        texturePath="character/knight/knight.png";
+        texturePath = "character/knight/knight.png";
+    }
+
+    public void setLevel(Level level) {
+        currentLevel = level;
+        position = level.getStartTile().getCoordinate().toPoint();
     }
 
     @Override
     public Point getPosition() {
-        return null;
+        return position;
     }
 
     @Override
