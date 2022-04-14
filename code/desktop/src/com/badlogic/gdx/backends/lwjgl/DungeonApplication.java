b31611be95bfa8c458a3fcf63c454cef1516af4f
Tobias Grothe <85501570+Tobias321@users.noreply.github.com>
2022-04-14 14:05:54 +0200

    remove DungeonApplication.java

--- code/desktop/src/com/badlogic/gdx/backends/lwjgl/DungeonApplication.java
+++
@@ -1,7 +0,0 @@
-package com.badlogic.gdx.backends.lwjgl;
-
-/**
- * An extended LwjglApplication which allows to define the Statuscode, which should be returned on
- * forceExit.
- */
-public class DungeonApplication {}
