1bb03daa3f351e291464c82447f66ac18db43aeb
Tobias Grothe <85501570+Tobias321@users.noreply.github.com>
2022-04-14 14:14:00 +0200

    update DesktopLauncher.java, set config attributes

--- code/desktop/src/desktop/DesktopLauncher.java
+++ code/desktop/src/desktop/DesktopLauncher.java
@@ -9,9 +9,13 @@ import tools.Constants;
 public final class DesktopLauncher {
     public static void run(MainController mc) {
         Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+        config.setWindowSizeLimits(Constants.WINDOW_WIDTH, Constants.WINDOW_HEIGHT, 1000, 1000);
+        // maxWidth: 1000 and maxHeight: 1000 affects the fullscreen behavior
         config.setForegroundFPS(Constants.FRAME_RATE);
+        config.setTitle(Constants.WINDOW_TITLE);
         config.setWindowIcon(Constants.LOGO_PATH);
         // config.disableAudio(true);
+        // uncomment this if you wish no audio
         new Lwjgl3Application(new LibgdxSetup(mc), config);
     }
 }
